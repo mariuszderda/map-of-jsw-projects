2fc50eb03f845309a512f458986e098a0ab281f8
Mariusz Derda <mariusz.derda@gmail.com>
2023-05-10 21:39:11 +0200

    added map, pins, and list of project sidebar

---
+++ script/main.js
@@ -0,0 +1,209 @@
+// import { properties } from "./properties.js";
+
+const centerMap = [49.964882, 18.6047696]
+
+const map = L.map('map').setView(centerMap, 10)
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19 }).addTo(map)
+
+const jswIcon = L.icon({
+  iconUrl: '/assets/map-pin.svg',
+  shadowUrl: '/assets/map-pin-shadow.svg',
+
+  iconSize: [38, 95], // size of the icon
+  shadowSize: [50, 64], // size of the shadow
+  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62],  // the same for the shadow
+  popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+})
+
+const popupContent = (name) => {
+  const content = document.createElement("div");
+
+  content.classList.add("property");
+  content.innerHTML = `
+    <div class="icon">
+        <div class="fa-building">
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.0" width="434.000000pt" height="434.000000pt" viewBox="0 0 434.000000 434.000000" preserveAspectRatio="xMidYMid meet">
+            <g transform="translate(0.000000,434.000000) scale(0.100000,-0.100000)" fill="#fff" stroke="none">
+        <path d="M2879 4193 c-155 -110 -277 -211 -408 -338 -339 -329 -551 -695 -639 -1104 -25 -117 -27 -143 -26 -356 0 -136 4 -240 11 -255 9 -22 11 -18 17 35 57 459 277 854 661 1184 126 108 197 161 291 215 97 55 126 82 143 133 8 23 16 125 21 253 5 157 11 226 23 258 9 23 13 42 8 42 -4 0 -50 -30 -102 -67z"/>
+        <path d="M1402 3923 c-120 -249 -178 -416 -227 -658 -35 -175 -49 -337 -42 -510 16 -390 123 -720 342 -1050 74 -112 257 -335 275 -335 6 0 -12 44 -39 98 -101 201 -183 460 -213 671 -16 110 -15 372 1 476 35 223 113 448 219 625 86 144 106 196 100 255 -8 67 -65 176 -160 305 -99 134 -132 189 -149 248 -7 23 -15 42 -19 42 -3 0 -43 -75 -88 -167z"/>
+        <path d="M3700 3643 c-204 -35 -515 -163 -754 -311 -220 -136 -457 -341 -574 -497 -60 -80 -139 -233 -148 -285 l-5 -35 43 50 c238 276 487 459 778 570 100 39 192 60 347 80 116 14 190 40 231 78 13 12 46 66 74 119 33 64 71 121 115 168 l65 70 -74 -1 c-40 -1 -84 -4 -98 -6z"/>
+        <path d="M3235 3015 c-89 -14 -240 -51 -264 -65 -10 -6 33 -10 119 -10 221 0 397 -38 540 -117 78 -42 124 -77 207 -155 57 -54 65 -58 109 -58 26 0 82 12 125 26 65 22 93 25 159 22 44 -2 80 0 80 3 0 11 -103 104 -170 153 -131 97 -316 172 -490 201 -112 18 -303 18 -415 0z"/>
+        <path d="M5 2384 c22 -122 118 -372 215 -564 137 -269 307 -500 534 -725 444 -440 996 -725 1621 -836 116 -21 161 -24 410 -24 203 0 286 3 300 12 18 11 17 12 -17 13 -78 0 -278 42 -406 85 -363 121 -685 327 -993 634 -258 259 -366 418 -538 793 -175 382 -158 369 -566 442 -228 41 -385 95 -524 182 l-44 27 8 -39z"/>
+    </g>
+        </svg>
+        </div>
+        <span class="fa-sr-only">${ name }</span>
+    </div>
+    <div class="details">
+        <h2 class="minor" data-mine="${ name }">${ name }</h2>
+    
+    </div>
+    `;
+  return content
+}
+
+const projectsOnMap = (projects) => {
+  const programsInUnit = []
+  projects.forEach(el => {
+    const programsIndex = programsInUnit.findIndex(el => el.programs)
+    if (el.program) {
+      if (programsIndex >= 0) {
+        const programIndex = programsInUnit[programsIndex].programs.findIndex(element => element.name === el.program)
+        programIndex >= 0
+          ? programsInUnit[programsIndex].programs[programIndex].projects.push({
+            name: el.name
+          })
+          : programsInUnit[programsIndex].programs.push({
+            name: el.program,
+            projects: [
+              {
+                name: el.name
+              }
+            ]
+          })
+      } else {
+        programsInUnit.unshift({
+          programs: [
+            {
+              name: el.program,
+              projects: [
+                {
+                  name: el.name
+                }
+              ]
+            }
+          ]
+        })
+      }
+    } else {
+      const projectsIndex = programsInUnit.findIndex(el => el.projects)
+      !!!programsInUnit[projectsIndex]
+        ? programsInUnit.push({ projects: [{ name: el.name }] })
+        : programsInUnit[projectsIndex].projects.push({ name: el.name })
+    }
+  })
+  return programsInUnit
+}
+
+const createHTMLElement = (elementHTML, innerHTMLElement = '', className = '') => {
+
+  const element = document.createElement(elementHTML)
+  if (className) {
+    element.classList.add(className)
+  }
+  if (innerHTMLElement) {
+    element.innerHTML = innerHTMLElement
+  }
+  return element
+}
+
+const createSideBar = (name) => {
+  const programsRaw = properties.find(item => item.mine === name)
+  const programsSort = projectsOnMap(programsRaw.projects)
+
+  const oldContainer = document.querySelector('.container')
+  if (oldContainer) {
+    oldContainer.remove()
+  }
+
+  const titleWithLogo = (name) => {
+    return `
+        <div class="title__container">
+            <div class="logo">
+              <svg class="logo" xmlns="http://www.w3.org/2000/svg" version="1.0" viewBox="0 0 434.000000 434.000000" preserveAspectRatio="xMidYMid meet">
+                <metadata>
+                    Created by potrace 1.14, written by Peter Selinger 2001-2017
+                </metadata>
+                <g transform="translate(0.000000,434.000000) scale(0.100000,-0.100000)" fill="#fff" stroke="none">
+                    <path d="M2879 4193 c-155 -110 -277 -211 -408 -338 -339 -329 -551 -695 -639 -1104 -25 -117 -27 -143 -26 -356 0 -136 4 -240 11 -255 9 -22 11 -18 17 35 57 459 277 854 661 1184 126 108 197 161 291 215 97 55 126 82 143 133 8 23 16 125 21 253 5 157 11 226 23 258 9 23 13 42 8 42 -4 0 -50 -30 -102 -67z"/>
+                    <path d="M1402 3923 c-120 -249 -178 -416 -227 -658 -35 -175 -49 -337 -42 -510 16 -390 123 -720 342 -1050 74 -112 257 -335 275 -335 6 0 -12 44 -39 98 -101 201 -183 460 -213 671 -16 110 -15 372 1 476 35 223 113 448 219 625 86 144 106 196 100 255 -8 67 -65 176 -160 305 -99 134 -132 189 -149 248 -7 23 -15 42 -19 42 -3 0 -43 -75 -88 -167z"/>
+                    <path d="M3700 3643 c-204 -35 -515 -163 -754 -311 -220 -136 -457 -341 -574 -497 -60 -80 -139 -233 -148 -285 l-5 -35 43 50 c238 276 487 459 778 570 100 39 192 60 347 80 116 14 190 40 231 78 13 12 46 66 74 119 33 64 71 121 115 168 l65 70 -74 -1 c-40 -1 -84 -4 -98 -6z"/>
+                    <path d="M3235 3015 c-89 -14 -240 -51 -264 -65 -10 -6 33 -10 119 -10 221 0 397 -38 540 -117 78 -42 124 -77 207 -155 57 -54 65 -58 109 -58 26 0 82 12 125 26 65 22 93 25 159 22 44 -2 80 0 80 3 0 11 -103 104 -170 153 -131 97 -316 172 -490 201 -112 18 -303 18 -415 0z"/>
+                    <path d="M5 2384 c22 -122 118 -372 215 -564 137 -269 307 -500 534 -725 444 -440 996 -725 1621 -836 116 -21 161 -24 410 -24 203 0 286 3 300 12 18 11 17 12 -17 13 -78 0 -278 42 -406 85 -363 121 -685 327 -993 634 -258 259 -366 418 -538 793 -175 382 -158 369 -566 442 -228 41 -385 95 -524 182 l-44 27 8 -39z"/>
+                </g>
+              </svg>
+            </div>
+            <h2>${ name }</h2>
+        </div>    
+`
+  }
+
+  const titleHTML = createHTMLElement('div', titleWithLogo(programsRaw.mine), '')
+  const container = createHTMLElement('div', '', 'container')
+
+  container.appendChild(titleHTML)
+
+  programsSort.forEach(el => {
+    // console.log('el1', el)
+    if (el.programs) {
+      const title = createHTMLElement('h3', 'Programy:', '')
+      container.appendChild(title)
+      const programList = createHTMLElement('ul', '', 'program__list')
+
+      el.programs.forEach(pr => {
+
+        const projectList = createHTMLElement('ul', '', 'project__list')
+        const programTitle = createHTMLElement('h5', pr.name, 'program__title')
+        const programItem = createHTMLElement('li', '', 'program__item')
+
+        programItem.appendChild(programList)
+        programList.appendChild(programTitle)
+
+        pr.projects.forEach(project => {
+          const projectItem = createHTMLElement('li', project.name, 'project__item')
+          projectList.appendChild(projectItem)
+        })
+        programList.appendChild(projectList)
+      })
+      container.appendChild(programList)
+    } else if (el.projects) {
+      const projectList = createHTMLElement('ul', '', 'project__list')
+      const title = createHTMLElement('h3', 'Projekty:', '')
+      container.appendChild(title)
+      el.projects.forEach(project => {
+        const projectItem = createHTMLElement('li', project.name, 'project__item')
+        projectList.appendChild(projectItem)
+      })
+      container.appendChild(projectList)
+      console.log(el.projects)
+    }
+  })
+  document.body.appendChild(container)
+}
+
+for (const element of properties) {
+  const marker = L.marker([element.position.lat, element.position.lng], { icon: jswIcon }).addTo(map)
+
+  marker.setLatLng([element.position.lat, element.position.lng])
+  marker.bindPopup(popupContent(element.mine))
+
+  const titleClick = marker._popup._content.querySelector('h2')
+
+  titleClick.addEventListener('click', function () {
+    createSideBar(element.mine)
+    marker.closePopup()
+  })
+
+  const property = marker._popup._content
+
+  property.addEventListener('mouseleave', function () {
+    marker.closePopup()
+  })
+
+  marker.on('mouseover', function (e) {
+    this._popup._content.classList.add('highlight')
+    this.openPopup()
+  })
+
+  // TODO close popup if mouse not enter on popup
+  // marker.on('mouseout', function (e) {
+  //   const popup = document.querySelector('.leaflet-popup ')
+  //
+  //   console.log(!!popup)
+  //   if (!!popup && mouseOnPopup) {
+  //   this.closePopup()
+  //   }
+  // })
+}
---
+++ script/properties.js
@@ -0,0 +1,100 @@
+const properties = [
+  {
+    mine: "KWK BUDRYK",
+    position: {
+      lat: 50.17049574519758,
+      lng: 18.76921844809755
+    },
+    projects: [
+      {
+        name: 'project ccc',
+      },
+      {
+        name: 'project aaa',
+        program: 'program ddd'
+      },
+      {
+        name: 'project bbb',
+        program: 'program ddd'
+      },
+      {
+        name: 'project extra',
+        program: 'program aaa'
+      },
+      {
+        name: 'project aaa',
+      },
+    ]
+  },
+  {
+    mine: "Ruch Borynia KWK Borynia-Zofiówka-Bzie",
+    position: {
+      lat: 49.99858593095107,
+      lng: 18.630257296934918
+    },
+    projects: [
+      {
+        name: 'aaa',
+        program: 'program ddd'
+
+      },
+      {
+        name: 'bbb'
+      }
+    ]
+  },
+  {
+    mine: "KWK Knurów-Szczygłowice Ruch Szczygłowice",
+    position: {
+      lat: 50.18776431315807,
+      lng: 18.63657478319555
+    },
+    projects: [
+      {
+        name: 'aaa',
+        program: 'ddd'
+
+      },
+      {
+        name: 'bbb',
+        program: 'ddd'
+
+      },
+      {
+        name: 'bbb'
+      }
+    ]
+  },
+  {
+    mine: "KWK Krupiński",
+    position: {
+      lat: 50.05232508784127,
+      lng: 18.77435799019109
+    },
+    projects: [
+      {
+        name: 'aaa',
+        program: 'ddd'
+
+      },
+      {
+        name: 'bbb'
+      }
+    ]
+  },
+  {
+    mine: "Jakaś tam",
+    position: {
+      lat: 49.96619300469611,
+      lng: 18.688541091918324
+    },
+    projects: [
+      {
+        name: 'bbb'
+      },
+      {
+        name: 'xxx'
+      },
+    ]
+  },
+];
